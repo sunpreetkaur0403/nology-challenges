7418d360f08476226a74075aa38c1a83e60733d6
Sunpreet Kaur <sunpreet@192-168-1-102.tpgi.com.au>
2023-06-05 12:45:46 +1000

    adds operators challenge files

---
+++ js/operators/script.js
@@ -0,0 +1,34 @@
+// Challenge: Working with ages
+let age = 28;
+output = age + 10;
+console.log(output);
+
+output = age - 7;
+console.log(output);
+
+output = age * 11;
+console.log(output);
+
+// Challenge: Calculating Moon Rotations
+
+let noOfEarthDays = 50;
+let daysToRotate = 27.3;
+let noOfRotations = noOfEarthDays / daysToRotate ;
+let noOfMoonRotations = `Number of rotations of the moon in ${noOfEarthDays} earth days is ${noOfRotations}`; 
+console.log(noOfMoonRotations);
+
+
+// Challenge: Calculating Moon Orbits
+
+let daysToOrbit = 27;
+let noOfOrbits = noOfEarthDays / daysToOrbit ;
+let noOfMoonOrbits = `Number of rotations of the moon in ${noOfEarthDays} earth days is ${noOfOrbits}`;
+console.log(noOfMoonOrbits);
+
+
+
+
+
+
+
+
