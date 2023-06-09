29fc4ebf7e142321cccc3a2c67605f78674bfce9
Sunpreet Kaur <sunpreet@192-168-1-102.tpgi.com.au>
2023-06-09 11:36:14 +1000

    adds arrays challenges files

---
+++ js/arrays/arrays.js
@@ -0,0 +1,17 @@
+const hobbies = ["Watching Netflix","Listening to music", "Reading books"];
+console.log(hobbies[0]);
+
+const firstHobby = hobbies.pop();
+
+const arrayLength = hobbies.length;
+
+hobbies.push("Hiking");
+console.log(hobbies[hobbies.length - 1]);
+
+
+console.log(`My hobbies are: ${hobbies.join(", ")}`);
+
+
+const groceryItems = ["apple", "pear", "papaya", "mango"];
+
+console.log(groceryItems.join("|"));
