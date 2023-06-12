8c49ef79515efee0aeea56b34ef198767e526295
Sunpreet <sunpreet@192-168-1-102.tpgi.com.au>
2023-06-13 00:12:38 +1000

    adds loops challenges file

---
+++ js/loops/README.md
@@ -0,0 +1,70 @@
+# Loops challenges
+
+## Challenge: Sum of n
+
+MVP:
+Create a function with a for loop that will add all the numbers up to n
+
+You should create a variable to set the upper limit of your loop
+Your loop should increment by 1 on each iteration
+Your loop should print the final result
+
+```
+sum(10) => 55
+```
+
+## Challenge: Write a shopping list
+
+MVP:
+Create a function with a for loop that will add numbers to your shopping list
+
+Store the following array in a variable
+Your loop should iterate through every value in the array
+Your loop should print a shopping list with the number and the name of the item
+
+```
+['milk', 'watermelon', 'eggs', 'soap', 'butter', 'apples', 'ice-cream'];
+=>
+// In Console
+
+1. milk
+2. watermelon
+3. eggs
+4. soap
+5. butter
+6. apples
+7. ice-cream
+```
+
+## Challenge: Capitalizing Odd Positioned Letters
+
+MVP
+Create a variable with a string of your choice
+Loop through the letters in this string and build a new string
+If the current index is odd, capitalize the letter before adding it to the new string
+Bonus
+If the current index is even increment the letter
+E.g: a becomes b, d becomes e, t becomes u
+Final: z becomes a
+
+# Challenge: Removing Vowels
+
+MVP
+Create a variable with a string of your choice
+Loop through the letters in this and build a new string
+The new strings should be the same as the input with the vowels missing
+
+```
+E.g: calum => clm, rachel => rchl, martyna => mrtyn
+```
+
+Bonus
+Keep vowels in the new strings if they are succeeded by the letters l, m, or r
+
+```
+E.g: calum => calum, rachel => rchel, martyna => martyn
+```
+
+## Loop through an array backwards
+
+Console.log() every element from the array
---
+++ js/loops/loops.js
@@ -0,0 +1,106 @@
+//Create a function with a for loop that will add all the numbers up to n
+
+//You should create a variable to set the upper limit of your loop 
+//Your loop should increment by 1 on each iteration
+// Your loop should print the final result
+
+function sumOfN(n){
+  let sum = 0;
+  for (let i = 1; i <= n; i++){
+    sum += i;
+  }
+  console.log(`Sum of ${n} numbers is ${sum}`);
+}
+
+sumOfN(10);
+
+//Create a function with a for loop that will add numbers to your shopping list
+//Store the following array in a variable Your loop should iterate through every 
+//value in the array Your loop should print a shopping list with the number and
+// the name of the item
+
+
+
+function addNoToShoppingList(){
+  const shoppingList = ['milk', 'watermelon', 'eggs', 'soap', 'butter', 'apples', 'ice-cream'];
+  for(let i = 0; i < shoppingList.length; i++){
+    console.log(`${i+1}. ${shoppingList[i]}`);
+  }
+}
+addNoToShoppingList();
+
+
+
+
+//Create a variable with a string of your choice Loop through the letters 
+  //in this string and build a new string If the current index is odd, 
+  //capitalize the letter before adding it to the new string 
+  //Bonus If the current index is even increment the letter 
+  //E.g: a becomes b, d becomes e, t becomes u Final: z becomes a
+
+  function changeString(originalString) {
+    let newString = '';
+  
+    for (let i = 0; i < originalString.length; i++) {
+      let currentLetter = originalString[i];
+  
+      if (i % 2 === 0) { // Even index
+        if (currentLetter === 'z') {
+          newString += 'a';
+        } else {
+          newString += String.fromCharCode(currentLetter.charCodeAt(0) + 1);
+        }
+      } else { // Odd index
+        newString += currentLetter.toUpperCase();
+      }
+    }
+  
+    return newString;
+  }
+  
+
+  const originalString = 'sunpreet';
+  const changedString = changeString(originalString);
+  console.log(changedString); 
+  
+
+  //Create a variable with a string of your choice Loop through the letters in
+  // this and build a new string The new strings should be the same as the input
+  // with the vowels missing
+
+
+
+
+  function removeVowels(originalString) {
+    let newString = '';
+  
+    for (let i = 0; i < originalString.length; i++) {
+      let currentLetter = originalString[i].toLowerCase();
+  
+      if (!isVowel(currentLetter)) {
+        newString += originalString[i];
+      }
+    }
+  
+    return newString;
+  }
+  
+  function isVowel(letter) {
+    return ['a', 'e', 'i', 'o', 'u'].includes(letter);
+  }
+  
+  
+  const newString = removeVowels(originalString);
+  console.log(newString); 
+
+  /* ## Loop through an array backwards
+Console.log() every element from the array */
+
+const myHobbies = ["Netflix" , "Music" , "Hiking"];
+
+for(let i = myHobbies.length - 1 ; i >= 0 ; i--) {
+    console.log(myHobbies[i]);
+}
+  
+
+  
