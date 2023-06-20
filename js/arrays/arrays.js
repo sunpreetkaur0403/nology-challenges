d9e34f73efc57ae82cc61767dd6e26512a3b80e1
Sunpreet <sunpreet@192-168-1-101.tpgi.com.au>
2023-06-20 22:31:48 +1000

    arrays challenges file

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -1,17 +1,28 @@
-const hobbies = ["Watching Netflix","Listening to music", "Reading books"];
-console.log(hobbies[0]);
+// const hobbies = ["Watching Netflix","Listening to music", "Reading books"];
+// console.log(hobbies[0]);
 
-const firstHobby = hobbies.shift();
+// const firstHobby = hobbies.shift();
 
-const arrayLength = hobbies.length;
+// const arrayLength = hobbies.length;
 
-hobbies.push("Hiking");
-console.log(hobbies[hobbies.length - 1]);
-
-
-console.log(`My hobbies are: ${hobbies.join(", ")}`);
+// hobbies.push("Hiking");
+// console.log(hobbies[hobbies.length - 1]);
 
 
-const groceryItems = ["apple", "pear", "papaya", "mango"];
+// console.log(`My hobbies are: ${hobbies.join(", ")}`);
 
-console.log(groceryItems.join("|"));
+
+// const groceryItems = ["apple", "pear", "papaya", "mango"];
+
+// console.log(groceryItems.join("|"));
+
+
+let scoreArr =[10]
+let total = 0;
+export const totalScores = (scoreArr) => {
+for(let i=0; i < scoreArr.length; i++){
+  total += scoreArr[i];
+}
+  return total;
+};
+console.log(scoreArr);
